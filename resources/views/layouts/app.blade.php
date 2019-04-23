5a6e5a1f57aecfb7c6058c64d55b08eb7e71a757
Louise Salas <louise@Louises-MacBook-Pro.local>
2019-04-24 07:52:31 +0800

    Udpated builder

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,44 +43,6 @@
 
 	<link href="{{ asset('capstone/Template/assets/css/style.css')}}" rel="stylesheet">
 	<link href="{{ asset('capstone/Template/assets/css/responsive.css')}}" rel="stylesheet">
-
-	<script type="text/javascript">
-	'use strict';
-			if ('serviceWorker' in navigator) {
-					// Your service-worker.js *must* be located at the top-level directory relative to your site.
-					// It won't be able to control pages unless it's located at the same level or higher than them.
-					// *Don't* register service worker file in, e.g., a scripts/ sub-directory!
-					// See https://github.com/slightlyoff/ServiceWorker/issues/468
-					navigator.serviceWorker.register('/service-worker.js').then(function(reg) {
-					// updatefound is fired if service-worker.js changes.
-					  console.log('ServiceWorker registration successful with scope: ', reg.scope);
-					reg.onupdatefound = function() {
-					// The updatefound event implies that reg.installing is set; see
-					// https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#service-worker-container-updatefound-event
-					var installingWorker = reg.installing;
-					installingWorker.onstatechange = function() {
-					switch (installingWorker.state) {
-							case 'installed':
-								if (navigator.serviceWorker.controller) {
-								
-									console.log('New or updated content is available.');
-								} else {
-							// At this point, everything has been precached.
-							// It's the perfect time to display a "Content is cached for offline use." message.
-							console.log('Content is now available offline!');
-						}
-						break;
-			case 'redundant':
-				console.error('The installing service worker became redundant.');
-				break;
-				}
-				};
-			};
-			}).catch(function(e) {
-			console.error('Error during service worker registration:', e);
-			});
-			}
-</script>
 	@yield('styles')
 </head>
 
