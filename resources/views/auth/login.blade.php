b7c851beaed6d3bf12a913367d6904feee906185
Louise Salas <louise@Louises-MacBook-Pro.local>
2019-04-24 08:28:15 +0800

    Udpated builder

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -43,35 +43,41 @@
 	<link href="{{ asset('splashscreens/ipadpro1_splash.png')}}" media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
 	<link href="{{ asset('splashscreens/ipadpro3_splash.png')}}" media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
 	<link href="{{ asset('splashscreens/ipadpro2_splash.png')}}" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
-  <!-- <script type="text/javascript">
+  <script type="text/javascript">
 	'use strict';
-			if
-					navigator.serviceWorker.register('/service-worker.js').then(function(reg) {
-					// updatefound is fired if service-worker.js changes.
-					  console.log('ServiceWorker registration successful with AGHSHS : ', reg.scope);
-					reg.onupdatefound = function() {
-					var installingWorker = reg.installing;
-					installingWorker.onstatechange = function() {
-					switch (installingWorker.state) {
-							case 'installed':
-								if (navigator.serviceWorker.controller) {
-
-									console.log('New or updated content is available.');
-								} else {
-								          console.log('Content is now available offline!');
-						}
-						break;
-			case 'redundant':
-				console.error('The installing service worker became redundant.');
-				break;
-				}
+				if ('serviceWorker' in navigator) {
+				navigator.serviceWorker.register('/pwa-builder.js').then(function(reg) {
+				  console.log('ServiceWorker registration successful with scope: ', reg.scope);
+				reg.onupdatefound = function() {
+								// The updatefound event implies that reg.installing is set; see
+								// https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#service-worker-container-updatefound-event
+								var installingWorker = reg.installing;
+								installingWorker.onstatechange = function() {
+												switch (installingWorker.state) {
+												case 'installed':
+															if (navigator.serviceWorker.controller) {
+																// At this point, the old content will have been purged and the fresh content will
+																// have been added to the cache.
+																// It's the perfect time to display a "New content is available; please refresh."
+																// message in the page's interface.
+																console.log('New or updated content is available.');
+															} else {
+																// At this point, everything has been precached.
+																// It's the perfect time to display a "Content is cached for offline use." message.
+																console.log('Content is now available offline!');
+															}
+													break;
+												case 'redundant':
+													console.error('The installing service worker became redundant.');
+													break;
+												}
+								};
 				};
-			};
-			}).catch(function(e) {
-			console.error('Error during service worker registration:', e);
-			});
+				}).catch(function(e) {
+				console.error('Error during service worker registration:', e);
+				});
 			}
-</script> -->
+</script>
 
 </head>
 
@@ -151,20 +157,20 @@
     // Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
 
     // Check compatibility for the browser we're running this in
-    if ("serviceWorker" in navigator) {
-          if (navigator.serviceWorker.controller) {
-          console.log("[PWA Builder] active service worker found, no need to register");
-          } else {
-          // Register the service worker
-          navigator.serviceWorker
-          .register("pwa-builder.js", {
-            scope: "./"
-          })
-          .then(function (reg) {
-            console.log("[PWA Builder] Service worker has been registered for scope: " + reg.scope);
-          });
-          }
-    }
+    // if ("serviceWorker" in navigator) {
+    //       if (navigator.serviceWorker.controller) {
+    //       console.log("[PWA Builder] active service worker found, no need to register");
+    //       } else {
+    //       // Register the service worker
+    //       navigator.serviceWorker
+    //       .register("pwa-builder.js", {
+    //         scope: "./"
+    //       })
+    //       .then(function (reg) {
+    //         console.log("[PWA Builder] Service worker has been registered for scope: " + reg.scope);
+    //       });
+    //       }
+    // }
 
     </script>
 
