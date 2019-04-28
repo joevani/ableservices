227a86b0d0e5ee4130c232441ae7c4f415ed02b9
louise93 <louisesalas8.26.com>
2019-04-28 19:47:48 +0800

    Updated na gyud

--- resources/views/location/assignment.blade.php
+++ resources/views/location/assignment.blade.php
@@ -18,7 +18,7 @@
                                               <ol class="breadcrumb">
                                                   <li>
                                                       <i class="fa fa-home"></i>
-                                                      <a class="parent-item" href="">Home</a>
+                                                      <a class="parent-item" href="{{URL::to('dashboard')}}">Home</a>
                                                       <i class="fa fa-angle-right"></i>
                                                   </li>
                                                   <li class="active">
