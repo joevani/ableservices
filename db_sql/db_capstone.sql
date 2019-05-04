2be4edd55337e6adeb5153d511b70b215414a702
louise93 <louisesalas8.26.com>
2019-05-04 17:32:20 +0800

    Uploaded

--- db_sql/db_capstone.sql
+++ db_sql/db_capstone.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 28, 2019 at 02:01 PM
+-- Generation Time: May 04, 2019 at 11:27 AM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.2.12
 
@@ -65,9 +65,11 @@ CREATE TABLE `chat` (
 --
 
 INSERT INTO `chat` (`id`, `from_user`, `to_user`, `message`, `status`, `created_at`, `updated_at`) VALUES
-(1, 1, 21, 'Okay', 0, NULL, NULL),
-(2, 21, 34, 'OKay', 0, '2019-04-28 05:10:30', '2019-04-28 05:10:30'),
-(3, 34, 29, 'dsd', 0, '2019-04-28 09:47:36', '2019-04-28 09:47:36');
+(7, 1, 35, 'Test', 0, '2019-04-28 23:34:46', '2019-04-28 23:34:46'),
+(8, 1, 35, 'WAHAHA', 0, '2019-04-28 23:42:21', '2019-04-28 23:42:21'),
+(9, 1, 16, 'sdsd', 0, '2019-05-02 09:17:14', '2019-05-02 09:17:14'),
+(10, 1, 36, 'okay', 0, '2019-05-02 09:41:08', '2019-05-02 09:41:08'),
+(11, 1, 36, 'Okay', 0, '2019-05-02 09:41:33', '2019-05-02 09:41:33');
 
 -- --------------------------------------------------------
 
@@ -108,16 +110,17 @@ CREATE TABLE `feedbacks` (
   `user_id` int(11) NOT NULL,
   `subject` varchar(255) NOT NULL,
   `content` text NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `is_read` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `feedbacks`
 --
 
-INSERT INTO `feedbacks` (`id`, `user_id`, `subject`, `content`, `created_at`) VALUES
-(1, 17, 'dsd', 'sdsd', '2019-04-27 11:04:36'),
-(2, 25, 'dd', 'dfd', '2019-04-27 12:03:32');
+INSERT INTO `feedbacks` (`id`, `user_id`, `subject`, `content`, `created_at`, `is_read`) VALUES
+(1, 17, 'dsd', 'sdsd', '2019-04-27 11:04:36', 0),
+(2, 25, 'dd', 'dfd', '2019-04-27 12:03:32', 0);
 
 -- --------------------------------------------------------
 
@@ -175,16 +178,17 @@ CREATE TABLE `memo` (
   `subject` varchar(255) NOT NULL,
   `content` text NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `created_by` int(11) NOT NULL
+  `created_by` int(11) NOT NULL,
+  `is_read` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `memo`
 --
 
-INSERT INTO `memo` (`id`, `user_id`, `subject`, `content`, `created_at`, `created_by`) VALUES
-(1, 17, 'dsd', 'sdsd', '2019-04-27 11:04:36', 1),
-(2, 25, 'oka', 'dfdf', '2019-04-27 12:03:07', 1);
+INSERT INTO `memo` (`id`, `user_id`, `subject`, `content`, `created_at`, `created_by`, `is_read`) VALUES
+(1, 17, 'dsd', 'sdsd', '2019-04-27 11:04:36', 1, 0),
+(2, 25, 'oka', 'dfdf', '2019-04-27 12:03:07', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -208,7 +212,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (3, '2019_04_22_194144_create_tickets_table', 1),
 (4, '2019_04_22_194217_create_categories_table', 1),
 (5, '2019_04_22_194258_create_comments_table', 1),
-(6, '2019_04_22_195348_create_chat_table', 1);
+(6, '2019_04_22_195348_create_chat_table', 1),
+(7, '2019_05_02_092140_create_notifications_table', 2);
 
 -- --------------------------------------------------------
 
@@ -235,9 +240,47 @@ CREATE TABLE `news` (
 --
 
 INSERT INTO `news` (`id`, `subject`, `content`, `thumbnail`, `client_can_see`, `supervisor_can_see`, `teamlead_can_see`, `admin_can_see`, `worker_can_see`, `created_at`, `updated_at`) VALUES
-(1, 'dsd', 'sd', NULL, 0, 0, 0, 0, 0, '2019-04-25 07:12:47', '2019-04-25 07:12:47'),
-(2, 'NEWS FOR EVERYPNE', 'DFD', NULL, 0, 1, 0, 1, 0, '2019-04-25 07:13:22', '2019-04-25 07:13:22'),
-(3, 'HAJGH', 'JJ', '1556177469.png', 0, 0, 0, 1, 0, '2019-04-25 07:31:09', '2019-04-25 07:31:09');
+(1, 'dsd', 'sd', NULL, 0, 0, 0, 0, 0, '2019-04-25 07:12:47', '2019-04-25 07:12:47');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notification`
+--
+
+CREATE TABLE `notification` (
+  `id` int(11) NOT NULL,
+  `title` varchar(225) NOT NULL,
+  `message` varchar(225) NOT NULL,
+  `is_opened` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notifications`
+--
+
+CREATE TABLE `notifications` (
+  `id` char(36) COLLATE utf8_unicode_ci NOT NULL,
+  `type` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `notifiable_type` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `notifiable_id` bigint(20) UNSIGNED NOT NULL,
+  `data` text COLLATE utf8_unicode_ci NOT NULL,
+  `read_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `notifications`
+--
+
+INSERT INTO `notifications` (`id`, `type`, `notifiable_type`, `notifiable_id`, `data`, `read_at`, `created_at`, `updated_at`) VALUES
+('1e5671ad-dda0-4ff5-8e8e-a4d7bdf6238c', 'App\\Notifications\\Register', 'App\\User', 36, '{\"title\":\"New Message\",\"message\":\"Okay\"}', '2019-05-01 16:00:00', '2019-05-02 01:41:33', '2019-05-02 01:41:33'),
+('4f5b7350-e4ab-4542-852d-5e43fae02b8f', 'App\\Notifications\\Register', 'App\\User', 1, '{\"data\":\"This is my first notification\"}', NULL, '2019-05-02 01:29:25', '2019-05-02 01:29:25'),
+('e383e9e6-bd67-405e-a35f-e60dac3b173a', 'App\\Notifications\\Register', 'App\\User', 1, '{\"title\":\"New Message\",\"message\":\"Hello world\"}', NULL, '2019-05-02 01:35:28', '2019-05-02 01:35:28'),
+('e3a25fd9-e6cb-41c4-bf03-9ad29de68aec', 'App\\Notifications\\Register', 'App\\User', 1, '{\"title\":\"New Message\",\"message\":\"Hello world\"}', NULL, '2019-05-02 01:35:07', '2019-05-02 01:35:07');
 
 -- --------------------------------------------------------
 
@@ -262,6 +305,7 @@ CREATE TABLE `reply` (
   `ticket_id` int(10) UNSIGNED NOT NULL,
   `user_id` int(10) UNSIGNED NOT NULL,
   `comment` text COLLATE utf8_unicode_ci NOT NULL,
+  `status` int(11) NOT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
@@ -270,11 +314,41 @@ CREATE TABLE `reply` (
 -- Dumping data for table `reply`
 --
 
-INSERT INTO `reply` (`id`, `ticket_id`, `user_id`, `comment`, `created_at`, `updated_at`) VALUES
-(8, 2, 34, 'test', NULL, NULL),
-(9, 2, 34, 'okay', NULL, NULL),
-(10, 2, 34, 'Test', NULL, NULL),
-(11, 3, 34, 'Okay', NULL, NULL);
+INSERT INTO `reply` (`id`, `ticket_id`, `user_id`, `comment`, `status`, `created_at`, `updated_at`) VALUES
+(13, 7, 1, 'Test', 1, '2019-04-28 23:34:46', '2019-04-28 23:34:46'),
+(14, 7, 35, 'okaysds', 0, '2019-04-28 23:37:29', '2019-04-28 23:37:29'),
+(15, 7, 1, 'Okay', 1, '2019-04-28 23:41:52', '2019-04-28 23:41:52'),
+(16, 8, 1, 'WAHAHA', 1, '2019-04-28 23:42:21', '2019-04-28 23:42:21'),
+(17, 7, 1, 'grani siya', 1, '2019-04-28 23:54:55', '2019-04-28 23:54:55'),
+(18, 7, 1, 'Bastos', 1, '2019-04-29 00:09:29', '2019-04-29 00:09:29'),
+(19, 7, 1, 'hpya', 1, '2019-04-29 00:27:01', '2019-04-29 00:27:01'),
+(20, 7, 35, 'wasap', 0, '2019-04-29 00:27:23', '2019-04-29 00:27:23'),
+(21, 9, 1, 'sdsd', 0, '2019-05-02 09:17:14', '2019-05-02 09:17:14'),
+(22, 10, 1, 'okay', 0, '2019-05-02 09:41:08', '2019-05-02 09:41:08'),
+(23, 11, 1, 'Okay', 0, '2019-05-02 09:41:33', '2019-05-02 09:41:33');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reports`
+--
+
+CREATE TABLE `reports` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` int(11) NOT NULL,
+  `is_read` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `reports`
+--
+
+INSERT INTO `reports` (`id`, `user_id`, `subject`, `content`, `created_at`, `created_by`, `is_read`) VALUES
+(1, 1, '1556557780.jpg', 'sas', '2019-04-29 17:09:40', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -389,7 +463,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `username`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `user_type`, `user_pic`, `is_deleted`, `address`, `dob`, `company`) VALUES
-(1, 'Louise Shit', 'adminsd', 'louisesalas@gmail.com', NULL, '$2y$10$nEZzofOy5m4O.Cvw6uImCegpdpcW34CgKFgMefvZ2i4AhrBxXKSs.', '3q2d28pOcl3rQHLj4MkIxCJjGxqP91eyFGhtBQdStLDUWNCNVWEZbdwyoMC7', NULL, NULL, 'management', '1556406263.jpg', 0, 'sd', '2019-04-19', 'N/A'),
+(1, 'Louise Shit', 'admin', 'louisesalas@gmail.com', NULL, '$2y$10$nEZzofOy5m4O.Cvw6uImCegpdpcW34CgKFgMefvZ2i4AhrBxXKSs.', '3q2d28pOcl3rQHLj4MkIxCJjGxqP91eyFGhtBQdStLDUWNCNVWEZbdwyoMC7', NULL, NULL, 'management', '1556406263.jpg', 0, 'sdhh', '2019-04-19', 'N/A'),
 (15, 'IMM COIN', 'sod', 'dsd@gmail.com', NULL, '$2y$10$nEZzofOy5m4O.Cvw6uImCegpdpcW34CgKFgMefvZ2i4AhrBxXKSs.', NULL, '2019-04-22 16:31:31', '2019-04-22 16:31:31', 'supervisor', 'capstone/Template/assets/images/about-1.jpg', 0, '', '0000-00-00', 'N/A'),
 (16, 'df', 'sds', 'ds', NULL, '$2y$10$IQdbGKl0Q5nHTdmad.Ee/.q/04hl9Su8NSKsCSKWsnEu8MLydVPXi', NULL, '2019-04-22 16:48:41', '2019-04-22 16:48:41', 'supervisor', 'capstone/Template/assets/images/about-1.jpg', 0, '', '0000-00-00', 'N/A'),
 (17, 'df', 'sd', 'df', NULL, '$2y$10$5etgmo.C1mUO4MtFxzJZ/.Ui4vedHwzJLl21rnlnh1IexDPdA4f/C', NULL, '2019-04-22 16:49:11', '2019-04-22 16:49:11', 'team lead', 'capstone/Template/assets/images/about-1.jpg', 0, '', '0000-00-00', 'N/A'),
@@ -409,7 +483,17 @@ INSERT INTO `users` (`id`, `name`, `username`, `email`, `email_verified_at`, `pa
 (31, 'Louise Salas', 'louise', 'ok@gmail.com', NULL, '$2y$10$JcnfQV7xuE7e4DF6KXgAfOn/h7L.qeXymyA8m8CgNMgMNTskc0Gma', 'bc9eF6a639A6FsnQNsPDn2UmO0Xb0qwPL8G5JAAfoTegTeq7nq9c1NPfTb2Z', NULL, NULL, 'team lead', '1556408015.jpg', 0, 'sdsd', '2019-04-10', 'N/A'),
 (32, 'Sample Client', 'sampleclient', 'sdsk@gmail.com', NULL, '$2y$10$mfAIn4/QV7CPrJLsSdRiYOM2I39qh3v7qgk9PZTgDgCFHcKtal0yW', NULL, NULL, NULL, 'worker', '1556411122.jpg', 0, 'Amba Business Hub, Trimandir, Adalaj,', '2019-04-18', 'Okay'),
 (33, 'Louise Salas', 'louise56', 'sfs@gmail.com', NULL, '$2y$10$tkJTSmcT8x.Sop0m1ZFmve..2Csjjhn5Jtcpfg5FVCdULbhXTzlJi', 'Qeo0Lu3eDKLKUDXLYDS6eYD3FZ61mXj6ensa3gHc2DnlIycZ3wuQ9wT94dUF', NULL, NULL, 'client', '1556423020.jpg', 0, 'Amba Business Hub, Trimandir, Adalaj,', '2019-04-11', 'SM City Cebu'),
-(34, 'dlfd', 'testclient', 'dg@gmail.com', NULL, '$2y$10$tn4OPlneTVYgCG5jXuNU5ehSPSWMgG4z93noIm3z3HTV7MAlUQ50e', 'Vr7QiNk376vxp2MnN5YBbylBRtGC7jZfEn7mNllc0cUGYU2WxCSRrgnMct2n', NULL, NULL, 'client', '1556423392.jpg', 0, 'sdsd', '2019-04-19', 'SM City Cebudsd');
+(34, 'dlfd', 'testclient', 'dg@gmail.com', NULL, '$2y$10$tn4OPlneTVYgCG5jXuNU5ehSPSWMgG4z93noIm3z3HTV7MAlUQ50e', 'Vr7QiNk376vxp2MnN5YBbylBRtGC7jZfEn7mNllc0cUGYU2WxCSRrgnMct2n', NULL, NULL, 'client', '1556423392.jpg', 0, 'sdsd', '2019-04-19', 'SM City Cebudsd'),
+(35, 'John Snow', 'testlouise', 'louisesalas826@gmail.com', NULL, '$2y$10$IZenArQXG.hw8niYkEmUKeb/8fBz5WQ6QjLhcaeMnIg6QfQ8MlYN2', NULL, NULL, NULL, 'supervisor', '1556493631.jpg', 0, 'Amba Business Hub, Trimandir, Adalaj,', '2019-04-29', 'N/A'),
+(36, 'Checker', 'check', 'louisesalas8.26@gmail.com', NULL, '$2y$10$pyRY.3CgcXjKX0iXqjH5KOEQeR3Ok6fCrxF871GXjxZnKmDA9ihh2', NULL, NULL, NULL, 'client', '1556788455.png', 0, 'sds', '2019-05-10', 'dsd'),
+(37, 'bjb', 'sbh', 'jj@gmail.com', NULL, '$2y$10$mQalO5THaLK9uQkIfTfKLOuFhGb8gGV/oZVStc6MQCfOx2OKTZEm2', NULL, NULL, NULL, 'supervisor', '1556945538.jpg', 0, 'dfdf', '2019-05-22', 'N/A'),
+(38, 'sds', 'sas', 'dsd@gm.con', NULL, '$2y$10$Vh7uK.kXFB92.QJBUSOS3OKh5BmGrh7SW2qfl9xnQ9flUjZshVHtO', NULL, NULL, NULL, 'supervisor', '1556945699.jpg', 0, 'dsd', '2019-05-29', 'N/A'),
+(39, 'ds', 'asasd', 'kk@gmail.com', NULL, '$2y$10$BCwfMeXAfihLoGsLAgh3fujYg3pOFfqoFME6nFh3YzRDteYh4358O', NULL, NULL, NULL, 'worker', '1556945853.jpg', 0, 'sdsd', '2019-05-09', 'N/A'),
+(40, 'okas', 'okas', 'louisesssalas8.26@gmail.com', NULL, '$2y$10$qg6iUjQNDP0C4npUBb3BLuhvfxaH.vTYTa4x.n/zSDI9nQmVOcmAa', NULL, NULL, NULL, 'team lead', '1556946096.jpg', 0, 'sd', '2019-05-16', 'N/A'),
+(41, 'ok', 'syaro', 'jaja@gmail.com', NULL, '$2y$10$1Du4bymAy6JKAxpA78cxrOnQylTJI.otbsmr56GWRtXLZrefbFSQu', NULL, NULL, NULL, 'worker', '1556946413.jpg', 0, 'sdk', '2019-05-09', 'N/A'),
+(42, 'JJK', 'SASJAHDJ', 'DJKHKHKH@GMAIL.COM', NULL, '$2y$10$s6md6Q4stMpu4l18ib02Muk7dhiA7nlSyJnsp.IpZFHp0ID9STCe6', NULL, NULL, NULL, 'supervisor', '1556946493.png', 0, 'DSD', '2019-05-17', 'N/A'),
+(43, 'dkshkh', 'grabi', 'hk@gmail.com', NULL, '$2y$10$TxQcMVtf5ss5LRpPrjoA6um88cQGuGhFJwsS2REfaNC6a1UKblRee', NULL, NULL, NULL, 'management', '1556946562.png', 0, 'sds', '2019-05-02', 'N/A'),
+(44, 'bwseit', 'bwesit', 'cyberspacsdse418@gmail.com', NULL, '$2y$10$DGGLsW6Ado2TDRKy/hVCfus8WBQd.nh.9o6XbcMN7JN75L9neDy86', NULL, NULL, NULL, 'management', '1556946706.png', 0, 'dsd', '2019-05-23', 'N/A');
 
 --
 -- Indexes for dumped tables
@@ -470,6 +554,19 @@ ALTER TABLE `news`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `notification`
+--
+ALTER TABLE `notification`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `notifications_notifiable_type_notifiable_id_index` (`notifiable_type`,`notifiable_id`);
+
+--
 -- Indexes for table `password_resets`
 --
 ALTER TABLE `password_resets`
@@ -479,6 +576,12 @@ ALTER TABLE `password_resets`
 -- Indexes for table `reply`
 --
 ALTER TABLE `reply`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `reports`
+--
+ALTER TABLE `reports`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -521,7 +624,7 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT for table `chat`
 --
 ALTER TABLE `chat`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `comments`
@@ -557,19 +660,31 @@ ALTER TABLE `memo`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `news`
 --
 ALTER TABLE `news`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `notification`
+--
+ALTER TABLE `notification`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `reply`
 --
 ALTER TABLE `reply`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT for table `reports`
+--
+ALTER TABLE `reports`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `supervisor_members`
@@ -593,7 +708,7 @@ ALTER TABLE `tickets`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
