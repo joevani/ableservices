b2f329c233255858b71374e1bd80e1ed212cd9d6
louise93 <louisesalas8.26.com>
2019-05-22 12:29:46 +0800

    updated status

--- resources/views/includes/flash.blade.php
+++ resources/views/includes/flash.blade.php
@@ -10,14 +10,6 @@
 
 
 
-@if (request()->session()->get('status'))
-    <div class="alert alert-success">
-        {{ request()->session()->get('status') }}
-        @if(request()->session()->get('password'))
-            <p class="text-danger">{{request()->session()->get('password')}}</p>
-        @endif
-    </div>
-@endif
 
 @if (session('status'))
     <div class="alert alert-success">
