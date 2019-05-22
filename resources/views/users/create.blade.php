4127012322940c445f3868bb174c0875e1ee11a4
louise93 <louisesalas8.26.com>
2019-05-22 12:13:39 +0800

    updated status

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -41,7 +41,7 @@
                                                   </div>
                                                   <div class="card-body">
                                                     @include('includes.flash')
-                                                    @include('includes.flashmessage')
+                                                  
                                                       @if ( $errors->any() )
                                                                @foreach ($errors->all() as $error)
                                                                 <div class="alert alert-danger">{{$error}}</div>
